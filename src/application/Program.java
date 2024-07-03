657a9a09222b920ad37564d3d521e8591bd28191
Andy <anderson54644@gmail.com>
2024-07-03 19:29:45 -0300

    create SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 		Department department = new Department(1, "Books");
 
 		Seller seller = new Seller(321, "Anderson", "anderson@gmail.com", new Date(), 3000.0, department);
+		
+		SellerDAO sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 	}
 
